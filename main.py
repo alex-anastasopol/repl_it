d70cc00fd632f23b0b5a5751c582e189080f6396
alex-anastasopol <anastasopolalex@gmail.com>
2020-04-21 10:13:22 +0000

    moved highest even function to its own file

--- main.py
+++
@@ -1,9 +0,0 @@
-def highest_even(li):
-    highest = li[0]
-    for i in li:
-        if i % 2 == 0 and i > highest:
-            highest = i
-    return highest
-
-
-print(highest_even([10, 2, 3, 4, 8, 11]))
