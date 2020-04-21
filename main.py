8a2f84ffd7a5887cc97bd946e5076f9384d31c0c
alex-anastasopol <anastasopolalex@gmail.com>
2020-04-21 10:10:44 +0000

    repl.it: python highest even method

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 def highest_even(li):
-  highest=li[0]
-  for i in li:
-    if i%2==0 and i > highest:
-      highest=i
-  return highest
+    highest = li[0]
+    for i in li:
+        if i % 2 == 0 and i > highest:
+            highest = i
+    return highest
 
-print(highest_even([10,2,3,4,8,11]))
+
+print(highest_even([10, 2, 3, 4, 8, 11]))
